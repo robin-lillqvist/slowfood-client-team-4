b91da70682fbaaed8a80526d6ca7bbc31a48c118
Robin Lillqvist <robin.lillqvist@gmail.com>
2020-03-05 13:22:13 +0100

    Login feature moved from faulty branch

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+// import './App.css'
+import LoginForm from './components/LoginForm'
+import { authenticate } from './modules/auth'
+
+class App extends Component {
+  state = {
+    renderLoginForm: false,
+    authenticated: false,
+    message: ''
+  }
+
+  onLogin = async e => {
+    e.preventDefault()
+    const response = await authenticate(
+      e.target.email.value,
+      e.target.password.value
+    )
+    if (response.authenticated) {
+      this.setState({ authenticated: true })
+    } else {
+      this.setState({ message: response.message, renderLoginForm: false })
+    }
+  }
+
+  render () {
+    let renderLogin
+
+    const { renderLoginForm, authenticated, message } = this.state
+
+    switch (true) {
+      case renderLoginForm && !authenticated:
+        renderLogin = <LoginForm submitFormHandler={this.onLogin} />
+        break
+      case !renderLoginForm && !authenticated:
+        renderLogin = (
+          <>
+            <button
+              id='login'
+              onClick={() => this.setState({ renderLoginForm: true })}
+            >
+              Login
+            </button>
+            <p id="message">{message}</p>
+          </>
+        )
+        break
+      case authenticated:
+        renderLogin = (
+          <p id="message">Hi {JSON.parse(sessionStorage.getItem('credentials')).uid}</p>
+        )
+        break
+    }
+
+    return (
+      <>
+        <h1>Slowfood</h1>
+        {renderLogin}
+      </>
+    )
+  }
+}
+export default App
