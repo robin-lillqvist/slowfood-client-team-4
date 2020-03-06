b5a41c8b92943c98ab658916ad8df946bd658180
Robin Lillqvist <robin.lillqvist@gmail.com>
2020-03-06 10:26:30 +0100

    trying to get cypress tests working

--- cypress/integration/userCanLogin.feature.js
+++ cypress/integration/userCanLogin.feature.js
@@ -4,6 +4,18 @@ describe("User authenticates", () => {
     });
 
 it("successfully with valid credentials", () => {
+    before(() => {
+        cy.server();
+        cy.route({
+            method: 'POST',
+            url: "http://localhost:3000/api/v1/auth/sign_in",
+            response: 'fixture:login.json',
+            headers: {
+                uid: 'user1@mail.com'
+            }
+        });
+    })
+
     cy.get("#login").click();
     cy.get("#login-form").within(() => {
         cy.get("#email").type("user@mail.com");
